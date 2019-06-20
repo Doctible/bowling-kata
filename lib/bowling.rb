9ba8e1b910399b6194aadd10c59451b1b501a25c
Reed Law <reed@doctible.com>
2019-06-20 11:49:29 -0700

    solve game

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,9 +1,38 @@
 class Bowling
   def initialize(input)
-    @input = input
+    @frames = input.split(' ')
+    puts input
   end
 
   def total_score
-    300
+    @frames.each_with_index.map do |frame, i|
+      frame_score(frame, i)
+    end.sum
+  end
+
+  def frame_score(frame, i)
+    if frame == 'X'
+      10 + (i >= 9 ? 0 : next_rolls(2, i + 1))
+    elsif frame.include?('/')
+      10 + (i >= 9 ? 0 : next_rolls(1, i + 1))
+    else
+      frame.chars.map(&:to_i).sum
+    end
+  end
+
+  def next_rolls(roll_count, i)
+    return 0 if @frames.count <= i
+
+    if roll_count == 1 # spare
+      roll = @frames[i].chars[0]
+      roll == 'X' ? 10 : roll.to_i
+    else # strike
+      if @frames[i].chars.count == 2
+        @frames[i].include?('/') ? 10 :
+          @frames[i].chars.map(&:to_i).sum
+      else
+        10 + next_rolls(1, i + 1)
+      end
+    end
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,9 +1,64 @@
 require 'rspec'
+require 'bowling'
 
 RSpec.describe Bowling do
   describe 'perfect game' do
     it 'should return 300' do
-      expect { subject.new('X X X X X X X X X XXX').total_score }.to eq 300
+      expect(described_class.new('X X X X X X X X X X X X').total_score).to eq 300
+    end
+  end
+
+  describe 'lousy game' do
+    it do
+      expect(described_class.new('1- 1- 1- 1- 1- 1- 1- 1- 1- 1-').total_score).to eq 10
+    end
+  end
+
+  describe 'slightly better lousy game' do
+    it do
+      expect(described_class.new('11 1- 1- 1- 1- 1- 1- 1- 1- 1-').total_score).to eq 11
+    end
+  end
+
+  describe 'even better lousy game' do
+    it do
+      expect(described_class.new('1/ 1- 1- 1- 1- 1- 1- 1- 1- 1-').total_score).to eq 20
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 1-').total_score).to eq 132
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('5/ 5/ 5/ 5/ 5/ 5/ -- 5/ 5/ 1-').total_score).to eq 112
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-').total_score).to eq 90
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('X 5/ 1- 1- 1- 1- 1- 1- 1- 1-').total_score).to eq 39
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('X 11 1- 1- 1- 1- 1- 1- 1- 1-').total_score).to eq 22
+    end
+  end
+
+  describe 'decent game' do
+    it do
+      expect(described_class.new('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5').total_score).to eq 150
     end
   end
 end
